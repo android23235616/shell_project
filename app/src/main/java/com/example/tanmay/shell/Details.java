b3fe5d34b17de929b166fe798f6fd4c57ae329f8
Tanmay Majumdar <tanmaycrazy@hotmail.com>
2016-12-03 21:28:40 +0530

    first commit

---
+++ app/src/main/java/com/example/tanmay/shell/Details.java
@@ -0,0 +1,193 @@
+package com.example.tanmay.shell;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class Details extends AppCompatActivity {
+
+    CircleImageView profile_pic;
+    Toolbar toolbar;
+    TextView Next, Back,Name,toolBarTitle;
+    Spinner hostel_spinner;
+    EditText phone;
+    Intent i;
+    LinearLayout editText_wrapper, spinner_wrapper;
+    Animation blink,slide_left;
+    private int tracker=0;
+    String hostel_name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        initialise();
+        spinner_wrapper.setVisibility(View.GONE);
+
+        i = getIntent();
+        if(i!=null){
+            setData(i);
+        }else{
+            setNullData();
+        }
+        setAnimations(this.blink,this.Name);
+        Click();
+        setChildTypeface();
+        setUpSpinner();
+        spinnerItemClick();
+    }
+
+    private void spinnerItemClick() {
+       hostel_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+           @Override
+           public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+               hostel_name = parent.getItemAtPosition(position).toString();
+           }
+
+           @Override
+           public void onNothingSelected(AdapterView<?> parent) {
+
+           }
+       });
+    }
+
+    private void setUpSpinner() {
+        List<String> hostel_adapter = new ArrayList<>();
+        for(int i=1; i<=12; i++)
+            hostel_adapter.add("KP-"+i);
+        for(int i=1; i<=6; i++)
+            hostel_adapter.add("QC-"+i);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.spinner_text_view,hostel_adapter);
+        arrayAdapter.setDropDownViewResource(R.layout.spinner_text_view);
+        hostel_spinner.setAdapter(arrayAdapter);
+    }
+
+    private void setAnimations(Animation blink, View profile_pic) {
+        profile_pic.startAnimation(blink);
+    }
+
+    private void setNullData() {
+        Name.setText("Hello, Tanmay");
+        String path="http://cdn1.blog-media.zillowstatic.com/1/Robert-Pattinson-IMDb-bd3782.jpg";
+        Glide.with(this).load(path).into(profile_pic);
+    }
+
+    private void setChildTypeface(){
+        setTypeFace(this.Name);
+        setTypeFace(this.Back);
+        setTypeFace(this.Next);
+        setTypeFace(this.toolBarTitle);
+    }
+    private void Click(){
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tracker++;
+                TrackerVisibilityChecker(tracker);
+                setAnimations(slide_left, phone);
+                phone.setText("");
+                setAnimations(slide_left,hostel_spinner);
+                phone.setHint("Enter Room Number:  ");
+            }
+        });
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tracker--;
+                TrackerVisibilityChecker(tracker);
+                setAnimations(slide_left, hostel_spinner);
+                phone.setText("");
+                phone.setHint("Phone Number: ");
+                setAnimations(slide_left, phone);
+            }
+        });
+    }
+
+    private void TrackerVisibilityChecker(int tracker) {
+        if(tracker==0){
+            editText_wrapper.setVisibility(View.VISIBLE);
+            spinner_wrapper.setVisibility(View.GONE);
+            Next.setVisibility(View.VISIBLE);
+            Back.setVisibility(View.INVISIBLE);
+        }else if(tracker==1)
+        {
+            printLog("Reacher here at traker 1");
+            editText_wrapper.setVisibility(View.GONE);
+            spinner_wrapper.setVisibility(View.VISIBLE);
+            Next.setVisibility(View.VISIBLE);
+            Back.setVisibility(View.VISIBLE);
+        }else{
+            editText_wrapper.setVisibility(View.VISIBLE);
+            spinner_wrapper.setVisibility(View.GONE);
+            Next.setVisibility(View.GONE);
+            Back.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void printLog(String s) {
+        Log.i("info 1",s);
+    }
+
+    private void setData(Intent b) {
+        String url = b.getStringExtra("icon");
+
+        String n="null";
+        n = b.getStringExtra("name");
+        if (n==null) {
+            setNullData();
+        } else {
+            Name.setText("Hello, " + n);
+            Glide.with(this).load(url).into(profile_pic);
+        }
+    }
+
+    private void initialise(){
+        Name = (TextView)findViewById(R.id.name);
+        profile_pic =(CircleImageView)findViewById(R.id.profile_pic);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        phone = (EditText)findViewById(R.id.number);
+        Next = (TextView)findViewById(R.id.next);
+        Back = (TextView)findViewById(R.id.Previous);
+        toolBarTitle = (TextView)findViewById(R.id.toolbarTitle);
+        hostel_spinner = (Spinner)findViewById(R.id.hostel_spinner);
+        blink = AnimationUtils.loadAnimation(this,R.anim.blink);
+        slide_left = AnimationUtils.loadAnimation(this,R.anim.slide_left);
+        editText_wrapper = (LinearLayout)findViewById(R.id.edittext_wrapper);
+        spinner_wrapper = (LinearLayout)findViewById(R.id.spinner_wrapper);
+    }
+
+    private void setTypeFace(View v){
+
+        Typeface t = Typeface.createFromAsset(getAssets(), "fonts/android.ttf");
+        ((TextView)v).setTypeface(t);
+    }
+
+    private void remove_status_bar(){
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
---
+++ app/src/main/java/com/example/tanmay/shell/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.tanmay.shell;
+
+import android.content.Intent;
+import android.graphics.Point;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+    private SignInButton signInButton;
+    private GoogleApiClient googleApiClient;
+    private GoogleSignInOptions googleSignInOptions;
+    private final int SIGN_IN_CONST=1000;
+    private int height, width;
+    private ImageView background;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getDimensions();
+        initialise();
+        Click();
+        full_screen();
+        imageBackground();
+    }
+
+    private void display(String s){
+        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
+    }
+
+    private void getDimensions(){
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+         width = size.x;
+         height = size.y;
+    }
+
+    private void imageBackground(){
+        String path="http://www.hostelbelgrade.com/images/hostel-belgrade-eye-03.jpg";
+        int p = R.drawable.a;
+        Glide.with(this).load(path).into(background);
+    }
+    private void full_screen(){
+      //  requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    private void initialise(){
+
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        signInButton = (SignInButton)findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setScopes(googleSignInOptions.getScopeArray());
+        signInButton.setColorScheme(SignInButton.COLOR_DARK);
+        background = (ImageView)findViewById(R.id.background);
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,googleSignInOptions).build();
+
+    }
+
+    private void Click(){
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent i = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(i, SIGN_IN_CONST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode==RESULT_OK&&requestCode==SIGN_IN_CONST){
+            GoogleSignInResult acct = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(acct.isSuccess()){
+                GoogleSignInAccount account = acct.getSignInAccount();
+               // display("Name is : "+account.getDisplayName()+"\nEmail is: "+account.getEmail());
+                Intent i = new Intent(this,Details.class);
+                i.putExtra("name", account.getDisplayName());
+                i.putExtra("email",account.getEmail());
+                i.putExtra("icon",account.getPhotoUrl().toString());
+                startActivity(i);
+            }else{
+                display("Invalid id & password");
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        googleApiClient.connect();
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+}
