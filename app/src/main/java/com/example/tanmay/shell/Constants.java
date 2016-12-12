36bd723189fb9482199d68e5f440048ecf5900f4
Tanmay Majumdar <tanmaycrazy@hotmail.com>
2016-12-13 05:14:52 +0530

    Toolbar for upload activity added

--- app/src/main/java/com/example/tanmay/shell/Constants.java
+++ app/src/main/java/com/example/tanmay/shell/Constants.java
@@ -6,9 +6,9 @@ package com.example.tanmay.shell;
 public class Constants {
     public final static String FIREBASE_URL="https://shell-151216.firebaseio.com/";
     public final static String USER_REGISTRATION_URL="http://www.pranshooverma1234.site88.net/shell/userdata_registration.php";
-    public final static String SHARED_PREF="MySharedPref1997";
+    public final static String SHARED_PREF="MySharedPref1998";
     public static final String REGISTERED = "registered";
     public static final String UNIQUE_ID = "uniqueid";
-    public static final String USER_DATA_SHARED_PREF = "usersharedpref";
+    public static final String USER_DATA_SHARED_PREF = "usersharedpref1";
 
 }
