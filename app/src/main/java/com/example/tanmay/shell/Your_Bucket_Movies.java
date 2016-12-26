af497fb8548e55c37dfb43b4e052fa36a649b235
PRANSHOO VERMA <majumdartanmay68@gmail.com>
2016-12-26 10:21:07 +0530

    Manifest Correction

--- app/src/main/java/com/example/tanmay/shell/Your_Bucket_Movies.java
+++ app/src/main/java/com/example/tanmay/shell/Your_Bucket_Movies.java
@@ -14,7 +14,7 @@ import android.view.ViewGroup;
 public class Your_Bucket_Movies extends Fragment {
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.your_bucket_movies, container,false);
+    public View onCreateView(LayoutInflater infla, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return infla.inflate(R.layout.your_bucket_movies, container,false);
     }
 }
